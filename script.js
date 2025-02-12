ead4f7a8fc194c1104b8c40c6dc09eb2b9e13051
JAmang-k <746948281@qq.com>
2025-02-12 22:44:04 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ function sendMessage() {
         loadingElement.style.display = 'block';
     }
 
-    const apiKey = '你的API Key';
+    const apiKey = 'sk-eba85221a52e41c6acd7c35c0f2434bc';
     const endpoint = 'https://api.deepseek.com/chat/completions';
 
     const payload = {
